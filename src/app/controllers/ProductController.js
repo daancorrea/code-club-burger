07356f14c22e422a3b9c5a3fd22ee44d25c5bba4
Daniel Euclides Correa <daniel.123.correa@gmail.com>
2021-09-15 09:58:54 -0300

    added controllers and routes

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,20 @@
+import * as Yup from "yup";
+
+class ProductController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      price: Yup.number().required(),
+      category: Yup.string().required(),
+    });
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+
+    return response.status(200).json({ routs: "Ok, this data is correct" });
+  }
+}
+
+export default new ProductController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import ProductController from "./app/controllers/ProductController";
+import SessionController from "./app/controllers/SessionController";
 
 import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+
+routes.post("/session", SessionController.store);
+
+routes.post("/products", ProductController.store);
 
 export default routes;
