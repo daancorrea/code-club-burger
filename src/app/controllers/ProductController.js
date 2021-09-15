08ff622aa5538f478a9d5e418e161dab2f24fba7
Daniel Euclides Correa <daniel.123.correa@gmail.com>
2021-09-15 11:19:09 -0300

    added new items

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,5 @@
 import * as Yup from "yup";
+import Product from "../models/Product";
 
 class ProductController {
   async store(request, response) {
@@ -7,13 +8,29 @@ class ProductController {
       price: Yup.number().required(),
       category: Yup.string().required(),
     });
+
     try {
       await schema.validateSync(request.body, { abortEarly: false });
     } catch (err) {
       return response.status(400).json({ error: err.errors });
     }
+    const { filename: path } = request.file;
+    const { name, price, category } = request.body;
 
-    return response.status(200).json({ routs: "Ok, this data is correct" });
+    const product = await Product.create({
+      name,
+      price,
+      category,
+      path,
+    });
+
+    return response.status(201).json(product);
+  }
+
+  async index(request, response) {
+    const products = await Product.findAll();
+
+    return response.json(products);
   }
 }
 
