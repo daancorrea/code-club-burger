3e8c17efcd64911b07f02e3da2d5b63b6440290c
Daniel Euclides Correa <daniel.123.correa@gmail.com>
2021-09-21 15:01:10 -0300

    commit for continue with application

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
 import * as Yup from "yup";
 import User from "../models/User";
+import jwt from "jsonwebtoken";
+import authConfig from "../../config/auth";
 
 class SessionController {
   async store(request, response) {
@@ -30,6 +32,9 @@ class SessionController {
       email,
       name: user.name,
       admin: user.admin,
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     });
   }
 }
