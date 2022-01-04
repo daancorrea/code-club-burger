f0bbe2a54dcdffd0ed995bb81a220f0c3176e31a
Daniel Corrêa <daniel.123.correa@gmail.com>
2022-01-04 10:03:56 -0300

    new features

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -29,8 +29,9 @@ class CategoryController {
       if (categoryExist) {
         return response.status(400).json({ error: "Category already exist " });
       }
-      const { id } = await Category.create({ name });
+      const { filename: path } = request.file;
 
+      const { id } = await Category.create({ name, path });
       return response.status(201).json({ id, name });
     } catch (error) {
       console.log(error);
@@ -41,6 +42,47 @@ class CategoryController {
     const category = await Category.findAll();
 
     return response.json(category);
+  }
+  async update(request, response) {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string(),
+      });
+
+      try {
+        await schema.validateSync(request.body, { abortEarly: false });
+      } catch (err) {
+        return response.status(400).json({ error: err.errors });
+      }
+      const { admin: isAdmin } = await User.findByPk(request.userId);
+
+      if (!isAdmin) {
+        return response.status(401).json();
+      }
+      const { name } = request.body;
+      const { id } = request.params;
+
+      const category = await Category.findByPk(id);
+
+      if (!category) {
+        return response
+          .status(401)
+          .json({ error: "Make sure your category ID is correct" });
+      }
+
+      let path;
+      if (request.file) {
+        path = request.file.filename;
+      }
+
+      const newCategory = await Category.update(
+        { name, path },
+        { where: { id } }
+      );
+      return response.status(201).json(newCategory);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -9,6 +9,7 @@ class ProductController {
       name: Yup.string().required(),
       price: Yup.number().required(),
       category_id: Yup.number().required(),
+      offer: Yup.boolean(),
     });
 
     try {
@@ -23,13 +24,14 @@ class ProductController {
     }
 
     const { filename: path } = request.file;
-    const { name, price, category_id } = request.body;
+    const { name, price, category_id, offer } = request.body;
 
     const product = await Product.create({
       name,
       price,
       category_id,
       path,
+      offer,
     });
 
     return response.status(201).json(product);
@@ -47,6 +49,54 @@ class ProductController {
     });
 
     return response.json(products);
+  }
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean(),
+    });
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false });
+    } catch (err) {
+      return response.status(400).json({ error: err.errors });
+    }
+    const { admin: isAdmin } = await User.findByPk(request.userId);
+
+    if (!isAdmin) {
+      return response.status(401).json();
+    }
+    const { id } = request.params;
+    const product = await Product.findByPk(id);
+
+    if (!product) {
+      return response
+        .status(401)
+        .json({ error: "Make sure your product ID is correct" });
+    }
+
+    let path;
+    if (request.file) {
+      path = request.file.filename;
+    }
+    const { name, price, category_id, offer } = request.body;
+
+    const product = await Product.update(
+      {
+        name,
+        price,
+        category_id,
+        path,
+        offer,
+      },
+      {
+        where: { id },
+      }
+    );
+
+    return response.status(201).json(product);
   }
 }
 
